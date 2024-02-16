41663dc3a44c036bfa605e61b7404ec1983abf86
TarunRamkumar <thepeeps33@gmail.com>
2024-02-15 21:56:41 -0500

    Finished project 2

---
+++ prog2-processor/src/CLI.py
@@ -0,0 +1,66 @@
+#@author Tarun
+#contains the command line 
+import argparse
+import FileParser
+
+mainParser = argparse.ArgumentParser(description="")
+
+#adding commands
+
+subparsers = mainParser.add_subparsers(dest="command",help="Available commands")
+
+#search command
+searchCommand = subparsers.add_parser("sc", help="searches through the file")
+
+#search command arguments
+group = searchCommand.add_mutually_exclusive_group(required=True)
+searchCommand.add_argument("-c", "--company", type=int, help="Choose company file to search. '1' for Google and '2' for GM", required=True)
+group.add_argument("-p","--part", type=str, help="Part of the 10k to search.")
+group.add_argument("-i","--item", type=str, help="Subsection of part to print")
+
+#table of contents command
+tocCommand = subparsers.add_parser("toc", help="Prints out the table of contents of the file")
+searchCommand.add_argument("-c", "--company", type=int, help="Company to display toc for. '1' for Google and '2' for GM", required=True)
+
+#prints out the entire file
+allCommand = subparsers.add_parser("ac", help="Prints out the entire file")
+allCommand.add_argument("-c", "--company", type=int, help="Company to display toc for. '1' for Google and '2' for GM", required=True)
+
+#parsing arguments
+args = mainParser.parse_args()
+
+#creates two file parsers, one for each company
+google = FileParser.FileParser("prog2-processor\data\Google10k-4Q-2024.txt")
+generalMotors = FileParser.FileParser("prog2-processor\data\GeneralMotors10k-4Q-2024.txt")
+
+#processes through the arguments and gives the corresponding output
+if args.company != 1 or 2:
+    print("Error: Invalid company detected. Retry the command, ensuring you either input 1 or 2 for the company")
+
+elif args.company == 1:
+    if args.command == "sc":
+        if args.part:
+            print(google.searchParts(args.part))
+        elif args.item:
+            print(google.searchItems(args.item))
+            
+    elif args.command == "ac":
+        print(google.getFile())
+    
+    elif args.command == "toc":
+        print(google.tableOfContents)
+     
+elif args.company == 2:
+    if args.command == "sp":
+        if args.part:
+            print(generalMotors.searchParts(args.part))
+        elif args.item:
+            print(generalMotors.searchItems(args.item))
+            
+    elif args.command == "ac":   
+        print(generalMotors.getFile())
+
+    elif args.command == "toc":
+        print(generalMotors.tableOfContents)
+
+
--- prog2-processor/src/FileParser.py
+++ prog2-processor/src/FileParser.py
@@ -1,5 +1,127 @@
+#@author Tarun
 class FileParser:
+#Creates a new FileParser object
+    def __init__(self,fileName):
+        
+        self.fileName = fileName
+        #formats the file and removes any escape characters and whitespaces 
+        with open(self.fileName, "r+", encoding="utf-8") as file:
+            content = file.read()
+            content = self.formatFile(content)
+        with open(self.fileName, "w", encoding="utf-8") as file:
+            file.write(content)
+        self.listTOC = self.setTOC()
+        self.tableOfContents = self.groupTOC(self.listTOC)
+         
+
+    def formatFile(self, content):
+        formattedContent = content.replace(u'\xa0', u' ').strip()
+        return formattedContent
+    #Creates the table of contents, but keeps it as a list rather than a dictionary
+    def setTOC(self):
+        tableOfContents = []
+        with open(self.fileName, "r", encoding="utf-8") as file:
+            started = False
+            for line in file:
+                if "PART I" in line.strip():
+                    started = True
+                elif started and "Table of Contents" in line.strip():
+                    print("done")
+                    break
+                elif started and not line.strip().isnumeric():
+                    tableOfContents.append(line.strip())
+        mergedTOC = []
+        i = 0
+        #Merges the items with their headings/descriptions
+        while i < len(tableOfContents):
+            if "Item" in tableOfContents[i]:
+                mergedTOC.append(tableOfContents[i] + " " + tableOfContents[i+1])
+                i+=2
+            else:
+                mergedTOC.append(tableOfContents[i])
+                i+=1
+        #return self.groupTOC(mergedTOC)
+        return mergedTOC
     
-    @staticmethod
-    def search(regex):
-        s
+    #Groups the TOC together into a dictionary, with the parts including all their children items 
+    def groupTOC(self,currentTOC):
+        groupedTOC = {"Part 0":[]}
+        currentPart = ""
+        for item in currentTOC:
+            if "PART" in item:
+                currentPart = item
+                groupedTOC[currentPart] = []
+            else:
+                if currentPart:
+                    groupedTOC[currentPart].append(item)
+                else:
+                    groupedTOC["Part 0"].append(item)
+        return groupedTOC
+    #Searches the file for each part, and returns the entire part
+    def searchParts(self,part):
+        result = ""
+        
+        toc = self.tableOfContents
+        if part.upper() in toc.keys():
+            listKeys = list(toc.keys())
+            if listKeys.index(part.upper()) == len(listKeys)-1:
+                nextItem = "None"
+            else:
+                nextItem = listKeys.pop(listKeys.index(part.upper())+1)
+            with open(self.fileName, "r", encoding="utf-8") as file:
+                started = False
+                started2 = False
+                for line in file:
+                    if started2 == False and part.upper() == line.strip():
+                        started2 = True
+                    elif started2 and part.upper() == line.strip():
+                        started = True
+                    elif started and line.strip().upper() == nextItem:
+                        return result
+                    elif started and nextItem == "None":                        
+                        result+=line
+                    elif started:
+                        result += line
+                return result
+        else:
+            return part + " not found in file. Please try again with another parameter. Valid values can be found using the 'toc' command"
+    
+    #Searches the items within each part, and returns them individually
+    def searchItems(self, item):
+        result = ""
+        toc = self.listTOC
+        
+        for string in toc:
+            if(item in string):
+                if toc.index(string) == len(toc) - 1:
+                    nextItem = "None"
+                else:
+                    nextItem = toc.pop(toc.index(string)+1)
+                    
+                with open(self.fileName, "r", encoding="utf-8") as file:
+                    started = False
+                    started2 = False
+                    for line in file:
+                        if started2 == False and item in line.strip():
+                            started2 = True
+                        elif started2 and item in line.strip():
+                            started = True
+                        elif started and line.strip().lower() in nextItem.lower():
+                            return result
+                        elif started and nextItem == "None":
+                            result+=line
+                        elif started:
+                            result += line
+                    return result
+        else:
+            return item + " not found in file. Please try again with another parameter. Valid values can be found using the 'toc' command"
+        
+    #Gets the entire file and returns it as a string
+    def getFile(self):
+        result = ""
+        with open(self.fileName, "r", encoding="utf-8") as file:
+            for line in file:
+                result += line
+        return result
+            
+                
